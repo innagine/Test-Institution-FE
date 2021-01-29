2e6ac6e8c5e51c26c0840a89a06bf3723282b73d
邓宁栋 <NingDongOne@163.com>
2021-01-29 20:06:52 +0800

    Update About.vue

--- test-institutions/src/views/About.vue
+++ test-institutions/src/views/About.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Description: file contentIMAGINE
+ * @Author: IMAGINE
+ * @Date: 2020-12-07 14:56:18
+ * @LastEditors: IMAGINE
+ * @LastEditTime: 2021-01-29 20:06:35
+-->
 <template>
   <div class="about">
-    <h1>This is an about page112233</h1>
+    <h1>This is an about page11223344</h1>
   </div>
 </template>
