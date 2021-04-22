8e408742b188547dd5f1d1e63b4b2fe33a402cc0
邓宁栋 <NingDongOne@163.com>
2021-04-22 10:44:40 +0800

    fix：更新机构完善资料页面下的机构图片模块

--- test-institutions/src/views/My-information/components/institution-complete.vue
+++ test-institutions/src/views/My-information/components/institution-complete.vue
@@ -3,16 +3,34 @@
  * @Author: IMAGINE
  * @Date: 2021-04-08 17:26:55
  * @LastEditors: IMAGINE
- * @LastEditTime: 2021-04-21 23:53:14
+ * @LastEditTime: 2021-04-22 10:43:20
 -->
 <template>
   <div class="institution-complete">
     <div class="ICcontent">
-      <div class="institution-information">1.添加/修改机构资料
+      <div class="institution-information">
+        1.添加/修改机构资料
         <div class="institution-avatar">
           <el-avatar :src="avartorUrl"></el-avatar>
         </div>
-        <div class="institution-picture">机构图片</div>
+        <div class="institution-picture">
+          机构图片
+          <el-image>
+            <div slot="error" class="image-slot">
+              <i class="el-icon-picture-outline"></i>
+            </div>
+          </el-image>
+          <el-image>
+            <div slot="error" class="image-slot">
+              <i class="el-icon-picture-outline"></i>
+            </div>
+          </el-image>
+          <el-image>
+            <div slot="error" class="image-slot">
+              <i class="el-icon-picture-outline"></i>
+            </div>
+          </el-image>
+        </div>
         <div class="institution-name">机构名称</div>
         <div class="institution-region">所属地区</div>
         <div class="institution-address">机构地址</div>
@@ -34,13 +52,14 @@ export default {
   name: "institution-complete",
   data() {
     return {
-      avartorUrl:'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
-    }
+      avartorUrl:
+        "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+    };
   },
   computed: {
     ...mapState(["user1", "baseUrl"])
   },
-  methods: {},
+  methods: {}
 };
 </script>
 
@@ -58,5 +77,4 @@ export default {
     margin: 50px auto;
   }
 }
-
 </style>
